753784e47a62cde02a0a7e53f1b3f59824641cd1
Ylianna <vyotskaya08@mail.ru>
2020-11-25 21:14:58 +0300

    54_less

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,69 +1,47 @@
 import React from "react";
 import styles from './users.module.css';
+import * as axios from 'axios';
+import userPhoto from '../../assets/images/icon-user-default.png'
 
-let Users = (props) => {
-
-    if (props.users.length === 0) {
-
-
-        props.setUsers([
-
-            {
-                id: 1,
-                photoUrl: 'https://lh3.googleusercontent.com/proxy/T7Sc0cmahculosmnQl7eVteFQZXB6rttiYwApxKiT_eSYo8TC-OHsqI59KNenIariDYrUUA1hLkLtt3qWQ'
-                ,
-                followed: false,
-                fullName: 'Vlad',
-                status: 'funny',
-                location: {city: 'Minsk', country: 'Belarus'}
-            },
-            {
-                id: 2,
-                photoUrl: 'https://lh3.googleusercontent.com/proxy/T7Sc0cmahculosmnQl7eVteFQZXB6rttiYwApxKiT_eSYo8TC-OHsqI59KNenIariDYrUUA1hLkLtt3qWQ'
-                ,
-                followed: true,
-                fullName: 'Sasha',
-                status: 'Hi',
-                location: {city: 'Moskow', country: 'Russia'}
-            },
-            {
-                id: 3,
-                photoUrl: 'https://lh3.googleusercontent.com/proxy/T7Sc0cmahculosmnQl7eVteFQZXB6rttiYwApxKiT_eSYo8TC-OHsqI59KNenIariDYrUUA1hLkLtt3qWQ'
-                ,
-                followed: false,
-                fullName: 'Dima',
-                status: 'I am cool',
-                location: {city: 'Kiev', country: 'Ukraine'}
-            }]
-        )
+class Users extends React.Component {
+    componentDidMount() {
+        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+            this.props.setUsers(response.data.items);
+        });
     }
 
-    return <div>
-        {
-            props.users.map(u => <div key={u.id}>
+    render() {
+        return   <div>
+            {
+                this.props.users.map(u => <div key={u.id}>
                 <span>
-                    <div> <img src={u.photoUrl} className={styles.usersPhoto}/></div>
+                    <div> <img src={u.photos.small != null ? u.photos.small : userPhoto} className={styles.usersPhoto}/></div>
                     <div>
-                        {u.followed ?<button onClick={() => {props.unfollow(u.id)} }> UnFollow </button > : <button onClick={() => {props.follow(u.id)}}> Follow </button>}
+                        {u.followed ? <button onClick={() => {
+                            this.props.unfollow(u.id)
+                        }}> UnFollow </button> : <button onClick={() => {
+                            this.props.follow(u.id)
+                        }}> Follow </button>}
 
                     </div>
 
                 </span>
-                <span>
+                    <span>
                  <span>
-                     <div>{u.fullName}</div>
+                     <div>{u.name}</div>
                      <div>{u.status}</div>
                  </span>
                  <span>
-                     <div>{u.location.country}</div>
-                     <div>{u.location.city}</div>
+                     <div>{"u.location.country"}</div>
+                     <div>{"u.location.city"}</div>
                  </span>
                 </span>
 
-            </div>)
-        }
-    </div>
+                </div>)
+            }
+        </div>
 
+    }
 }
 
 export default Users;
