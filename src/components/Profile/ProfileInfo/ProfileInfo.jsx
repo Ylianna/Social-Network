eca07989dbcceb50d41f0e2eafb8ef478700eefc
Ylianna <vyotskaya08@mail.ru>
2020-11-30 22:02:58 +0300

    60_less

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import s from './ProfileInfo.module.css';
+import Preloader from "../../common/Preloader/Preloader";
 
-const ProfileInfo = () => {
+const ProfileInfo = (props) => {
+    if (!props.profile) {
+        return  <Preloader />
+    }
     return (
         <div>
             <div>
@@ -9,7 +13,15 @@ const ProfileInfo = () => {
                     src='https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&h=350'/>
             </div>
             <div className={s.descriptionBlock}>
-                ava + description
+                <img src={props.profile.photos.large} />
+                {props.profile.aboutMe}
+                {props.profile.contacts.facebook}
+                {props.profile.contacts.website}
+                {props.profile.contacts.vk}
+                {props.profile.contacts.twitter}
+               <div>
+                   ava + description
+               </div>
             </div>
         </div>
     )
---
+++ src/components/common/Preloader/Preloader.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import preloader from '../../../assets/images/e9291eaddacd460280a34a151dcc5cc4.gif';
+
+
+let Preloader = (props) => {
+    return <div style={{backgroundColor: 'blue'}}>
+        <img src={preloader}/>
+    </div>
+
+}
+export default Preloader;
