1cdb5db8abf14f46ff52d8511933b4db684e12c0
Ylianna <vyotskaya08@mail.ru>
2020-12-14 22:40:12 +0300

    82_les

--- src/redux/users-selectors.js
+++ src/redux/users-selectors.js
@@ -1,11 +1,6 @@
-import {createSelector} from "reselect";
-
-const getUsersSelector = (state) => {
+export const getUsers = (state) => {
     return state.usersPage.users
 }
-export const getUsers = createSelector(getUsersSelector, (users) => {
-    return users.filter(u => true)
-})
 export const getPageSize = (state) => {
     return state.usersPage.pageSize
 }
