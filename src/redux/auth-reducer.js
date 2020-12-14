6d8364229a4608194842bd34215d3772aad80ccd
Ylianna <vyotskaya08@mail.ru>
2020-12-14 20:07:27 +0300

    80_less

--- src/redux/auth-reducer.js
+++ src/redux/auth-reducer.js
@@ -29,7 +29,7 @@ export const setAuthUserData = (userId, email, login, isAuth) => ({type: SET_USE
         {userId, email, login, isAuth}  });
 
 export const getAuthUserData = () => (dispatch) => {
-    authAPI.me()
+    return authAPI.me()
         .then(response => {
             if (response.data.resultCode === 0) {
                 let {id, login, email} = response.data.data;
@@ -45,10 +45,10 @@ export const login = (email, password, rememberMe) => (dispatch) => {
             if (response.data.resultCode === 0) {
                 dispatch(getAuthUserData())
             } else {
-              let message = response.data.messages.length > 0 ? response.data.messages[0] : "Some error";
+                let message = response.data.messages.length > 0 ? response.data.messages[0] : "Some error";
                 dispatch(stopSubmit("login", {_error: message}));
-            }
 
+            }
         });
 }
 
