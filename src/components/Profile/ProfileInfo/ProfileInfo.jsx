94ac6043ddc5dd3d7f20e6b3e88c567270e6af34
Ylianna <vyotskaya08@mail.ru>
2021-01-05 21:10:21 +0300

    96_less

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -2,10 +2,17 @@ import React from 'react';
 import s from './ProfileInfo.module.css';
 import Preloader from "../../common/Preloader/Preloader";
 import ProfileStatusWithHooks from "./ProfileStatusWithHooks";
+import userPhoto from "../../../assets/images/user.png";
 
-const ProfileInfo = ({profile, status, updateStatus}) => {
+const ProfileInfo = ({profile, status, updateStatus, isOwner, savePhoto}) => {
     if (!profile) {
         return <Preloader />
+    }
+
+    const onMainPhotoSelected = (e) => {
+      if  (e.target.files.length) {
+          savePhoto(e.target.files[0]);
+      }
     }
 
     return (
@@ -15,7 +22,8 @@ const ProfileInfo = ({profile, status, updateStatus}) => {
                     src='https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&h=350'/>
             </div>*/}
             <div className={s.descriptionBlock}>
-                <img src={profile.photos.large} />
+                <img src={profile.photos.large || userPhoto} className={s.mainPhoto} />
+                { isOwner && <input type="file" onChange={onMainPhotoSelected}/>}
                 <ProfileStatusWithHooks status={status} updateStatus={updateStatus}/>
             </div>
         </div>
