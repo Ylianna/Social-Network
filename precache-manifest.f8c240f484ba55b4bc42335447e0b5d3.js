310db4085164a4b4da58f04b3c1ded57594c3791
Ylianna <67273279+Ylianna@users.noreply.github.com>
2021-01-28 20:32:36 +0300

    Add files via upload

---
+++ precache-manifest.f8c240f484ba55b4bc42335447e0b5d3.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ba1b36526a8abe6c34af9d197137c5b",
+    "url": "/Social_Network/index.html"
+  },
+  {
+    "revision": "6b7fb9afd0622fbb98c8",
+    "url": "/Social_Network/static/css/3.3b3a6ed5.chunk.css"
+  },
+  {
+    "revision": "911f3404d76d16209340",
+    "url": "/Social_Network/static/css/4.76492d1b.chunk.css"
+  },
+  {
+    "revision": "5eba3a68ba1f5bc0f0a3",
+    "url": "/Social_Network/static/css/main.35dce83e.chunk.css"
+  },
+  {
+    "revision": "e38b14cec73aaf74813d",
+    "url": "/Social_Network/static/js/2.d4e432cd.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/Social_Network/static/js/2.d4e432cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b7fb9afd0622fbb98c8",
+    "url": "/Social_Network/static/js/3.ce2efdef.chunk.js"
+  },
+  {
+    "revision": "911f3404d76d16209340",
+    "url": "/Social_Network/static/js/4.b5ecee64.chunk.js"
+  },
+  {
+    "revision": "5eba3a68ba1f5bc0f0a3",
+    "url": "/Social_Network/static/js/main.c65c5080.chunk.js"
+  },
+  {
+    "revision": "1ca7bc2acfc8eb990af8",
+    "url": "/Social_Network/static/js/runtime-main.78f713ac.js"
+  },
+  {
+    "revision": "6e7c9db7e1f100edac7c6e48714d2ea0",
+    "url": "/Social_Network/static/media/preloader.6e7c9db7.svg"
+  },
+  {
+    "revision": "8b1dbb1f4e4468b9cc31ced9c63fcfe7",
+    "url": "/Social_Network/static/media/user.8b1dbb1f.png"
+  }
+]);
